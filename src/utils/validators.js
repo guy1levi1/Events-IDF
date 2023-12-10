1dd44d32efb4cd4737d8a6c4509ebfb9d75cec86
guy1levi1 <81861472+guy1levi1@users.noreply.github.com>
2023-12-10 17:33:10 +0200

    update validator.js
    fix password bug

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -11,12 +11,17 @@ export const VALIDATOR_EXACLENGTH = (data, lng) => {
 
 export const VALIDATOR_PRIVATE_NUMBER = (value) => {
   return VALIDATOR_EXACLENGTH(value, 7) && !/\D/.test(value);
-}
+};
 
 export const VALIDATOR_FULLNAME = (value) => {
-  return VALIDATOR_MAXLENGTH(value, 50) && /^[א-ת']+(\s[א-ת']{1,}){1,2}$/.test(value);
-}
+  return (
+    VALIDATOR_MAXLENGTH(value, 50) && /^[א-ת']+(\s[א-ת']{1,}){1,2}$/.test(value)
+  );
+};
 
 export const VALIDATOR_PASSWORD = (value) => {
-  return VALIDATOR_MINLENGTH(value, 6) && /^[^\sא-ת!@#$%^&*()_+={}4\]:;<>,.?/"'\\`|]*$/.test(value);
-}
+  return (
+    VALIDATOR_MINLENGTH(value, 6) &&
+    /^[^\sא-ת!@#$%^&*()_+={}\]:;<>,.?/"'\\`|]*$/.test(value)
+  );
+};

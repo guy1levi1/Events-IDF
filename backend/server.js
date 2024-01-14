d2e0ee9835c8f26c30c960975e54b8f039c7ffa9
guy1levi1 <guy1levi1@gmail.com>
2024-01-14 16:15:29 +0200

    conntec frontend to backend need to continue sign up send prop and more

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,8 @@ const unapprovedUsersRoutes = require("./routes/unapprovedUsersRoutes");
 const eventRequestsRoutes = require("./routes/eventRequestsRoutes");
 const commandsRoutes = require("./routes//commandsRoutes");
 const db = require("./dbConfig");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 
 const app = express();
 app.use(cors());
@@ -32,11 +34,16 @@ app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
+
+
 app.use("/api/unapprovedUsers", unapprovedUsersRoutes);
 app.use("/api/users", usersRoutes);
 app.use("/api/events", eventsRoutes);
 app.use("/api/eventRequests", eventRequestsRoutes);
 app.use("/api/commands", commandsRoutes);
+
+app.use("/api/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 
 // Start the server
 app.listen(port, async () => {
---
+++ frontend/src/utils/api.js
@@ -0,0 +1,46 @@
+import axios from "axios";
+
+export const get = async (url, headers) => {
+  try {
+    const res = await axios.get(url, { headers });
+    if (res) {
+      console.log("success get");
+      return res;
+    }
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export const post = async (url, body, headers) => {
+  try {
+    const res = await axios.post(url, body, { headers });
+    if (res) {
+      console.log("success post");
+    }
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export const patch = async (url, body, headers) => {
+  try {
+    const res = await axios.patch(url, body, { headers });
+    if (res) {
+      console.log("success patch");
+    }
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export const del = async (url, headers) => {
+  try {
+    const res = await axios.delete(url, { headers });
+    if (res) {
+      console.log("success delete");
+    }
+  } catch (e) {
+    console.log(e);
+  }
+};

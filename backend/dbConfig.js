eb5d003679e0a471053cd8ece935f4f0d1ca1a76
guy1levi1 <guy1levi1@gmail.com>
2024-01-31 15:34:33 +0200

    add some env to upload to red net

--- backend/dbConfig.js
+++ backend/dbConfig.js
@@ -1,8 +1,16 @@
 const { Sequelize } = require("sequelize");
+require("dotenv").config();
 
-const db = new Sequelize("EventsIDF", "postgres", "Aa123456123456", {
-  dialect: "postgres", // Use 'postgres' as the dialect for PostgreSQL
-  host: "localhost", // Change this to your PostgreSQL server's host
+const DB_NAME = process.env.DB_NAME;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_HOST = process.env.DB_HOST;
+const DB_DIALECT = process.env.DB_DIALECT;
+const DB_PORT = process.env.DB_PORT;
+
+const db = new Sequelize(DB_NAME, DB_DIALECT, DB_PASSWORD, {
+  dialect: DB_DIALECT, // Use 'postgres' as the dialect for PostgreSQL
+  host: DB_HOST, // Change this to your PostgreSQL server's host
+  port: DB_PORT,
 });
 
 module.exports = db;
